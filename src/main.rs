e72c3c7fbbf4783b6a0f6ecc9a3a58437a058dc3
Shota Mishima <sm4mma@gmail.com>
2021-12-05 15:40:15 +0000

    first commit.

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
+}
