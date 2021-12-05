ff48129a29389b8fba1da0cc95e211324245cb99
shotamishima <sm4mma@gmail.com>
2021-12-05 16:37:56 +0000

    Add error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder, ResponseError};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum MyError {}
+
+impl ResponseError for MyError {}
+
 
 #[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index() -> Result<impl Responder, MyError> {
+    Ok(HttpResponse::Ok().body("Hello world!"))
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(), actix_web::Error> {
     HttpServer::new(|| App::new().service(index))
         .bind("0.0.0.0:8080")?
         .run()
-        .await
+        .await?;
+    Ok(())
 }
